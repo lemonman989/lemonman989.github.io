f61e96e54072a10b787f42214c698db82052c522
lemonman989 <lemonman9890@gmail.com>
2024-10-22 16:08:18 -0500

    Delete 20.interactive_Koepke directory

--- 20.interactive_Koepke/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('luteCatImage').addEventListener('click', function() {
-    const textOverlay = document.getElementById('textOverlay');
-    textOverlay.classList.toggle('hidden');  // Toggle visibility on click
-});
