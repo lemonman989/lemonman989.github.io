3bc3b5ccb4a9847bae6e588a82269e7c36bc1887
lemonman989 <lemonman9890@gmail.com>
2024-10-22 16:05:56 -0500

    Add files via upload

---
+++ 20.interactive_Koepke/script.js
@@ -0,0 +1,4 @@
+document.getElementById('luteCatImage').addEventListener('click', function() {
+    const textOverlay = document.getElementById('textOverlay');
+    textOverlay.classList.toggle('hidden');  // Toggle visibility on click
+});
